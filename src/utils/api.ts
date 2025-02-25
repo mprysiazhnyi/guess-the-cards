be9b1b04e5df7321f67f03cf830769a637a882d6
Maksym Prysiazhnyi <maksym.prysiazhnyi@valatron.com>
2025-02-25 13:46:07 +0100

    Fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 export const fetchRandomWord = async (): Promise<string> => {
   try {
-    const response = await fetch('https://api.api-ninjas.com/v1/randomword', {
-      headers: { 'X-Api-Key': 'dtF3yrt680hNHbcr3kxSYg==sk2qOIbhbImCvA1k' },
+    const response = await fetch(import.meta.env.VITE_API_URL, {
+      headers: { 'X-Api-Key': import.meta.env.VITE_API_KEY },
     });
     const data: { word: string } = await response.json();
     return data.word;
